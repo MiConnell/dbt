d5782a2ace9ca66631dd36fc752bd17a949520bf
Michael Connell <connellmp@gmail.com>
2022-12-03 18:46:54 +0000

    set up test config

--- models/aoc2022/core/day_01_part_01.sql
+++ models/aoc2022/core/day_01_part_01.sql
@@ -1,1 +1,15 @@
-select 2400 as out
+with input as (
+    select input
+    from 
+    {{ source('input', 'day01') }}
+),
+
+output as (
+    select 'a'
+),
+
+final as (
+    select input as out from input 
+)
+
+select * from final
